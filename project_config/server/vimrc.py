8c12d1c73fad3eb1da0eb83f01f0ccd1096b6825
GAME\xuzhang <zhangxukim@gmail.com>
2017-12-29 18:26:45 +0800

    工程下的path支持absolute path

--- project_config/server/vimrc.py
+++ project_config/server/vimrc.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 import os
 
@@ -6,8 +7,19 @@ py_folders = [
 	os.path.join('..', 'common_server'),
 ]
 
+post_folders = []
+
+
+def _to_abs_path(folder, work_folder=None):
+	work_folder = work_folder or os.path.realpath(os.path.dirname(__file__))
+	if not os.path.isabs(folder):
+		folder = os.path.join(work_folder, folder)
+	return folder if os.path.isdir(folder) else None
+
 
 def on_start_jedihttp():
 	work_folder = os.path.realpath(os.path.dirname(__file__))
 	for folder in py_folders:
-		sys.path.insert(0, os.path.join(work_folder, folder))
+		sys.path.insert(0, _to_abs_path(folder, work_folder))
+	for folder in post_folders:
+		sys.path.append(_to_abs_path(folder, work_folder))
