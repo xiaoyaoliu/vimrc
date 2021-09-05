7a3a3138c63b5fed9f2a8172b7787b7c331dad9c
zhangxu <zhangxukim@gmail.com>
2021-09-05 16:52:23 +0800

    ReplaceVirtualFunction，支持在替换函数中，调用老的被替换掉的函数

--- documents/cpp_samples/ReplaceVirtualFunction/ReplaceVirtualFunction.cpp
+++ documents/cpp_samples/ReplaceVirtualFunction/ReplaceVirtualFunction.cpp
@@ -1,23 +1,85 @@
 #include "ReplaceVirtualFunction.h"
 #include <iostream>
 #include <windows.h>
+#include <unordered_map>
+#include <string>
+#include <assert.h>
 using namespace std;
 
+struct OriginVirtualInfo
+{
+	size_t* mVirtualFunctionTable;
+	int mIndex;
+	size_t mOriginFuncMemAddr;
+};
+typedef std::unordered_map<std::string, OriginVirtualInfo> FuncTableType;
 
+FuncTableType NewFuncNameToOldFuncs;
 
-void DoMemReplace(size_t* virtualFunctionTable, int index, size_t newFunc)
+static const int PTR_BYTES = sizeof(size_t);
+
+void __DoMemReplace(size_t* virtualFunctionTable, int index, size_t newFunc, const char* new_funcname)
 {
 #if _DEBUG
 	cout << "vt addr " << hex << (size_t)virtualFunctionTable << endl;
 	cout << "vt func " << hex << newFunc << "\r\nold func:" << hex << virtualFunctionTable[index] << "\t" << index << endl;
 #endif
-	static const int PTR_BYTES = sizeof(size_t);
+	if (new_funcname != nullptr) {
+		std::string newFuncName = new_funcname;
+		assert(NewFuncNameToOldFuncs.find(newFuncName) == NewFuncNameToOldFuncs.end());
+		OriginVirtualInfo info;
+		{
+			info.mVirtualFunctionTable = virtualFunctionTable;
+			info.mIndex = index;
+			info.mOriginFuncMemAddr = virtualFunctionTable[index];
+		}
+		NewFuncNameToOldFuncs.emplace(std::make_pair(newFuncName, info));
+	}
+	
 	DWORD old;
 	VirtualProtect(virtualFunctionTable, PTR_BYTES, PAGE_READWRITE, &old);
 	virtualFunctionTable[index] = newFunc;
 	VirtualProtect(virtualFunctionTable, PTR_BYTES, old, &old);
+
+	
 	
 }
+
+
+size_t __PauseReplace(const char* new_funcname)
+{
+	size_t func = 0;
+	FuncTableType::iterator origInfos = NewFuncNameToOldFuncs.find(new_funcname);
+	if (origInfos == NewFuncNameToOldFuncs.end()) {
+		return func;
+	}
+	OriginVirtualInfo& info = origInfos->second;
+	func = info.mVirtualFunctionTable[info.mIndex];
+	DWORD old;
+	VirtualProtect(info.mVirtualFunctionTable, PTR_BYTES, PAGE_READWRITE, &old);
+	info.mVirtualFunctionTable[info.mIndex] = info.mOriginFuncMemAddr;
+	VirtualProtect(info.mVirtualFunctionTable, PTR_BYTES, old, &old);
+	return func;
+}
+
+void __ResumeReplace(const char* new_funcname, size_t new_func)
+{
+	FuncTableType::iterator origInfos = NewFuncNameToOldFuncs.find(new_funcname);
+	if (origInfos == NewFuncNameToOldFuncs.end()) {
+		return;
+	}
+	OriginVirtualInfo& info = origInfos->second;
+	DWORD old;
+	VirtualProtect(info.mVirtualFunctionTable, PTR_BYTES, PAGE_READWRITE, &old);
+	info.mVirtualFunctionTable[info.mIndex] = new_func;
+	VirtualProtect(info.mVirtualFunctionTable, PTR_BYTES, old, &old);
+}
+
+
+
+
+
+#pragma region TestCode
 
 void chengedPrintf()
 {
@@ -50,6 +112,19 @@ public:
 };
 
 
+class ReplaceA
+{
+public:
+	virtual void OverrideFun2()
+	{
+		auto a = (A*)this;
+		SuperCall(__FUNCTION__, a->fun2());
+		cout << "Member Function Changed and can use this now" << endl << endl;
+	}
+
+};
+
+
 void TestVirtualFunctionReplaceMain()
 {
 
@@ -59,10 +134,16 @@ void TestVirtualFunctionReplaceMain()
 	   new B2
 	};
 
-	ReplaceVirtualFunction(pObj[0], &B1::fun2, (uint64_t)chengedPrintf);
+	//ReplaceVirtualFunction(pObj[0], &B1::fun2, (uint64_t)chengedPrintf);
+	ReplaceA replObj;
+	size_t oldfun = ReplaceVirtualWithMember(pObj[0], B1::fun2, &replObj, ReplaceA::OverrideFun2);
+	ReplaceVirtualWithFunction(pObj[2], A::fun2, chengedPrintf);
 
 	for (A* pTmp : pObj) {
 		pTmp->fun2();
 	}
 	
 }
+
+
+#pragma endregion
--- documents/cpp_samples/ReplaceVirtualFunction/ReplaceVirtualFunction.h
+++ documents/cpp_samples/ReplaceVirtualFunction/ReplaceVirtualFunction.h
@@ -1,7 +1,8 @@
 #include "VirtualGetIndex.h"
 
+
 template <class T, typename F>
-int VTableIndex(F f)
+int __VTableIndex(F f)
 {
 	static struct VTableCounter
 	{
@@ -16,19 +17,53 @@ int VTableIndex(F f)
 	return (t->*getIndex)();
 }
 
-void DoMemReplace(size_t* VirtualFunctionTable, int index, size_t new_func);
+void __DoMemReplace(size_t* VirtualFunctionTable, int index, size_t new_func, const char* new_funcname);
 
 
-template <class T, typename F>
-void ReplaceVirtualFunction(T *obj, F f, size_t new_func)
+size_t __PauseReplace(const char* new_funcname);
+
+void __ResumeReplace(const char* new_funcname, size_t new_func);
+
+template <typename T, typename F>
+size_t __ReplaceVirtualFunction(T *obj, F f, size_t new_func, const char* new_funcname)
 {
 	// address of virtual function table
 	size_t vtfAddress = *(size_t*)obj; 
 	// virtual table ptr
 	size_t* pVirtualFunctionTable = (size_t*)vtfAddress;
-	const int func_index = VTableIndex<T>(f);
-	DoMemReplace(pVirtualFunctionTable, func_index, new_func);
+	const int func_index = __VTableIndex<T>(f);
+	size_t old_func = pVirtualFunctionTable[func_index];
+	__DoMemReplace(pVirtualFunctionTable, func_index, new_func, new_funcname);
+	return old_func;
 }
+
+
+template <typename TOld, typename FOld, typename TNew, typename FNew>
+size_t __ReplaceVirtualWithMember(TOld* obj, FOld f, TNew *new_obj, FNew new_func, const char* new_funcname)
+{
+	// address of virtual function table
+	size_t vtfAddress = *(size_t*)obj;
+	// virtual table ptr
+	size_t* pVirtualFunctionTable = (size_t*)vtfAddress;
+	const int func_index = __VTableIndex<TOld>(f);
+
+	const int new_func_index = __VTableIndex<TNew>(new_func);
+	size_t* NewVirtualFunctionTable = (size_t*)*(size_t*)new_obj;
+	size_t old_func = pVirtualFunctionTable[func_index];
+	__DoMemReplace(pVirtualFunctionTable, func_index, NewVirtualFunctionTable[new_func_index], new_funcname);
+	return old_func;
+}
+
+
+#define ReplaceVirtualWithMember(obj, f, new_obj, new_func) __ReplaceVirtualWithMember(obj, &f, new_obj, &new_func, #new_func);
+
+#define ReplaceVirtualWithFunction(obj, f, new_func) __ReplaceVirtualFunction(obj, &f, (size_t)new_func, #new_func);
+
+#define SuperCall(func_name, callSupers) {auto __CUR_FUNC_ADDR = __PauseReplace(func_name); \
+callSupers; \
+__ResumeReplace(func_name, __CUR_FUNC_ADDR);	\
+}
+
 
 // test funciton
 void TestVirtualFunctionReplaceMain();
