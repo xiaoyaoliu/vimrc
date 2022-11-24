2d78e41bd2f0d62fb1a435985fa63c5e1f78395f
dozhang <zhangxukim@qq.com>
2022-11-24 12:41:53 +0800

    disable quickEdit of Cmd.

--- install_ycm.bat
+++ install_ycm.bat
@@ -1,3 +1,7 @@
+@echo off
+
+rem ## disable QuickEdit of Console.
+reg add HKEY_CURRENT_USER\Console /v QuickEdit /t REG_DWORD /d 00000000 /f
 cd %USERPROFILE%\.vim\plugged\YouCompleteMe
 %USERPROFILE%\vimrc\bin\win32\python39\python.exe install.py --all
 Pause
